80a0bf30390ecc8b4ab3170fa20ba8903302088d
Scott Ellis <scott@jumpnowtek.com>
2016-08-30 10:59:22 -0400

    Revert heap alloc of qapp. Results in loss of cursor on exit for console

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication *app = new QApplication(argc, argv);
-	QShowFonts *w = new QShowFonts;
+	QApplication a(argc, argv);
+	QShowFonts w;
 
 #ifdef Q_OS_LINUX
-	w->showFullScreen();
+	w.showFullScreen();
 #else
-	w->show();
+	w.show();
 #endif
 
-	return app->exec();
+	return a.exec();
 }
